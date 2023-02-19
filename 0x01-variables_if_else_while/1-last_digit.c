4f5b7071b5b27003d7d29ca03777b79b62a8c443
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-19 12:42:40 -0800

    2 last 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 /**
  * main - entry point Last digit of n is greater than 5 or equal to zero or not
- * * return- always 0 (success)
+ * return- always 0 (success)
  */
 
 int main(void)
@@ -24,7 +24,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, k);
 	if (k == 0)
 		printf("Last digit of %d is %d and is 0\n", n, k);
-	if (k < 6 && k != 0)
+	if (k < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, k);
 	return (0);
 }
