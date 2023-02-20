4ca8c3f605403ddf92b6c4959a38c508d85fb239
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-20 03:18:46 -0800

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-/**new part
- *main alphabet inpu
- *return:always
- */
+
+
 int main(void)
 {
 		char c;
 
 		for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+			putchar(c);
+
 		putchar('\n');
+
 		return (0);
 
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ */
+ int main()
+{	
+	char alp;
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+
+	putchar('\n');
+
+	return 0;
+}
