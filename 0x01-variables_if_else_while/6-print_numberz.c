5510e15ecc1df23c16712ce77ae9225e1beb3e57
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-20 06:52:46 -0800

    l single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
