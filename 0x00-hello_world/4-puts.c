615d1fce2aace5cde47a124cfbb29896f88b366d
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-20 14:23:48 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  * progra ming 
  */
 int main(void)
-
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
