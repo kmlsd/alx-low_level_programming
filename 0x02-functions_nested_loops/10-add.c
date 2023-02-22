5141f9949ad67a048c1aa5b2d5f7184b0e453c43
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-22 10:04:40 -0800

    additon

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add- function that adds two integers and returns the result
+ * @n:integer part1
+ * @m:integer part2
+ * Return:addition of two no return sum
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
