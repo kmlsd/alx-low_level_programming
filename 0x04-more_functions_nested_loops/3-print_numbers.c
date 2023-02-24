1eb629366b87b7a6a798c9fd568ccca15976418f
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-24 12:03:07 -0800

    prints the numbers, from 0 to 9,

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers-func print no 0 up 9
+ *
+ * Return:alist of 0-9
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+}
