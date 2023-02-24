ac19c22559eb19d648cb51f3e3b4259ee0278c0b
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-24 15:21:02 -0800

    digonal shape

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,29 +7,23 @@
  */
 void  print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i, j;
 
-		for (i = 1; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-
-			{
-				if (j == i)
-
-					_putchar('\\');
-
-				else if (j < i)
-
-					_putchar(' ');
-			}
-			_putchar('\n');
-		}
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < i; j++)
+	{
+	_putchar(32);
+	}
+	_putchar(92);
+	}
 	}
 
 
