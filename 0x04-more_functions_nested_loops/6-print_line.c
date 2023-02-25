3effd1177fb6a39c8d04d2ec5328bfe933815b99
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-24 17:50:50 -0800

    2prt

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,7 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-	
+	_putchar('\n');	
 	}
 	else
 	{
@@ -17,10 +17,10 @@ void print_line(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-		_putchar('_');
+		_putchar(95);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
 
