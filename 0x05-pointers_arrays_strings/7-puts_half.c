d1120797ee455ac0a76d93742f48628cb072f802
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-02-27 14:04:20 -0800

    hat prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * puts_half-function that prints half of a string
+ * @str:string inputt
+ */
+void puts_half(char *str)
+{
+	int j = 0;
+	int i;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+	if (j % 2 == 1)
+	{
+	i = (j - 1) / 2;
+	i += 1;
+	}
+	else
+	{
+	i = j / 2;
+	}
+
+	for (; i < j; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+
+
+}
+
+
+
