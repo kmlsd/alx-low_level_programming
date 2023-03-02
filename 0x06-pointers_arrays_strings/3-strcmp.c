fa3e6aad2b6883454d64dcb6851c10fbb3b28425
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-03-02 10:00:33 -0800

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp-str  compare func
+ * @s1:string 1 to cmpr
+ * @s2:string 2 to cmpr
+ * Return:> = 0 < out put
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+
+	}
+
+	return (0);
+
+}
+
+
