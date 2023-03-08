94fe30132c652d74fb9740f336daea8be81516af
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-03-08 09:39:34 -0800

    void _puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include<stdio.h>
+#include <string.h>
+
+/**
+ *  * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+
+
+}
