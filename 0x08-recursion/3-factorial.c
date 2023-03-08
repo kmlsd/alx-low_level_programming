de187203437bb69eb1bcd387deff8c6b91cb8ffb
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-03-08 11:07:23 -0800

    update

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * factorial - out put factory 
+ * factorial - out put factory of integer
  * @n: input
  * Return: Always 0 (Success)
  */
+
 int factorial(int n)
 {
 	if (n < 0)
