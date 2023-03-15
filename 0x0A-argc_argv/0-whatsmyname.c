56bf0328225f760672b617c61335b3d5b98ad14b
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-03-15 15:31:10 -0700

    that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print  pr name
+ * @argv: array
+ * @argc:no of atgument
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+
+}
