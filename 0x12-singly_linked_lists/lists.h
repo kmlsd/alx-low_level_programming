aa23cb0b722eaa11c4275cb7d3b615ccf4350b8d
kamal <kmlsd475                  git config --global user.email kmlsd475>
2023-03-31 14:32:02 -0700

    up

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,14 @@
 #ifndef	LISTS_H
 #define LISTS_H
+#include<stdio.h>
+
+/**
+ * struct list_s - srtuct contain str and sing linklen: is str int meme
+ * @len:struct mememrs
+ * @str:srtuct pointer
+ * @next:next add pointer
+ *
+ */
 
 typedef struct list_s
 {
@@ -7,7 +16,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-int _putchar(char c); 
+int _putchar(char c);
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
