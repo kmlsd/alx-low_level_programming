dbf8a9f60d55fd7dd2afd23f6217e6bf37b622ab
kmlsd <kmlsd4757@gmail.com>
2023-06-07 12:49:33 -0700

    recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+ * _putchar-input or out put prg
+ * @c:is input char
+ *
+ * Return:always 1 or 0
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
+
