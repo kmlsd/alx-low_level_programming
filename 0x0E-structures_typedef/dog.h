e6d820e794f5ea57cbceffee13d1f84ffaac7865
kmlsd <kmlsd4757@gmail.com>
2023-06-20 13:23:49 -0700

    upd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,18 +8,11 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+
+struct dog
 {
 	char *name;
-	float age;
+	float  age;
 	char *owner;
-} dog_t;
-#endif
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+};
 #endif
