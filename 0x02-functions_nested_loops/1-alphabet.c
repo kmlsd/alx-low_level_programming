fec6471fc774f3430c958a92d0550fd6151353a6
kmlsd <kmlsd4757@gmail.com>
2023-10-31 10:39:12 -0700

    1-alp

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet-check the code
+ * print_alphabeit putchar print
+ *
+ * Return: Always 0.
+ */
+
+
+void  print_alphabet(void)
+{
+
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+
+
+
+
+}
