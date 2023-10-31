f028dfd2ad31fc815cc18b61a74dc19e0271341d
kmlsd <kmlsd4757@gmail.com>
2023-10-31 12:44:00 -0700

    prt to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98- that prints all natural numbers
+ * @n:is natural varb
+ * return:print n
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
