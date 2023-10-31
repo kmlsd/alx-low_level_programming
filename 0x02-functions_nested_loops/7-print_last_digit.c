b635749a5ef049d53fd24b63a968d85d9ade76a0
kmlsd <kmlsd4757@gmail.com>
2023-10-31 12:26:42 -0700

    prt last digt

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit-hat prints the last digit of a number
+ * @n:is input integer
+ * Return:values is the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int t = n % 10;
+
+	if (t < 0)
+		t *= -1;
+
+	_putchar(t + '0');
+
+	return (t);
+
+}
