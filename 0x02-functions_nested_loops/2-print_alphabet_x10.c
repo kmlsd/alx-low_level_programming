5d6f6ae9550ad5a3ce7052f625233a2ec89812a8
kmlsd <kmlsd4757@gmail.com>
2023-10-31 11:56:42 -0700

    2-alp x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10-input out head
+ * return:alway 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+
+	for (i = 0; i <= 9; i++)
+	{
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+
+		_putchar(c);
+	}
+	_putchar('\n');
+	}
+
+}
