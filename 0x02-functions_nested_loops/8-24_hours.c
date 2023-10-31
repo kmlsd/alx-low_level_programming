6767c528a68f0fd997ce0c5af78559c2af15668f
kmlsd <kmlsd4757@gmail.com>
2023-10-31 12:30:52 -0700

    24 hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer-prints every minute of the day of
+ *
+ * Return:always input
+ */
+void jack_bauer(void)
+{
+	int hr, mn;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (mn = 0; mn <= 59; mn++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((mn / 10) + '0');
+			_putchar((mn % 10) + '0');
+			_putchar('\n');
+
+		}
+	}
+}
