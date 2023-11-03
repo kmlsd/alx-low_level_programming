e1d1dc27aea303e8570790456c222d15aa3bb2c4
kmlsd <kmlsd4757@gmail.com>
2023-11-03 15:36:01 -0700

    prt most no

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers-func print no 0 up 9 exc 2&4
+ *
+ * Return:alist of 0-9
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+
+}
