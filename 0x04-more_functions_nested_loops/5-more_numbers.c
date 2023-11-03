b0aafc8e1755baa3f3632efbe79aed315df685b5
kmlsd <kmlsd4757@gmail.com>
2023-11-03 15:38:53 -0700

    more no

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers- that prints 10 times the numbers, from 0 to 14,
+ *
+ * Return:alist of 0-9
+ */
+void more_numbers(void)
+{
+	int a, c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a >= 10)
+				_putchar('1');
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+
+	}
+}
