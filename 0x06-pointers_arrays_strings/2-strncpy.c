a5b9550c6cc7e69110ac99e118d5b05a93c75b73
kmlsd <kmlsd4757@gmail.com>
2023-11-08 09:29:45 -0800

    n copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy-str copy func
+ * @dest:destination str
+ * @src:source str
+ * @n:n is int no of index of source that can be copy
+ * Return: return is dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
