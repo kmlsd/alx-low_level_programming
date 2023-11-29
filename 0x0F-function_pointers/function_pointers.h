16d95ee651f4dfa9c8b2be3beb5609649ec5b828
kmlsd <kmlsd4757@gmail.com>
2023-11-29 13:29:59 -0800

    prt name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+typedef __SIZE_TYPE__ size_t;
+#endif
