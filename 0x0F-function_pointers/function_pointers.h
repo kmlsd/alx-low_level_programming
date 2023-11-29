7c18f921a3a63ca99b9240279f9c867f6f85bd62
kmlsd <122817589+kmlsd@users.noreply.github.com>
2023-11-29 13:12:26 -0800

    Delete 0x0F-function_pointers directory

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-#include <stddef.h>
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-typedef __SIZE_TYPE__ size_t;
-#endif
-
