989a4f57f15216516d1a1f6bfa9d50eb23187213
kmlsd <kmlsd4757@gmail.com>
2024-02-02 15:30:56 +0000

    update

---
+++ 0x18-dynamic_libraries/integ_op.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+* add - add two interger
+* @a: first integer
+* @b: second integ
+* Return: sum of a & b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* sub - sub two interger
+* @a: first integer
+* @b: second integ
+* Return: sub of a & b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* mul - multwo interger
+* @a: first integer
+* @b: second integ
+* Return: mul of a & b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* div - div two interger
+* @a: first integer
+* @b: second integ
+* Return: div of a & b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* mol - mol two interger
+* @a: first integer
+* @b: second integ
+* Return: mol of a & b
+*/
+int mol(int a, int b)
+{
+	return (a % b);
+}
